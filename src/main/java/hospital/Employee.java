2702fe7091da90e1378918f7bb524d35403bb592
Sivert <siveral@stud.ntnu.no>
2021-03-05 09:47:40 +0100

    Implemented Employee class.

---
+++ src/main/java/hospital/Employee.java
@@ -0,0 +1,25 @@
+package hospital;
+
+public class Employee extends Person {
+
+    /**
+     * Creates an instance of an employee.
+     *
+     * @param firstname            The first name of the employee.
+     * @param lastName             The last name of the emloyee.
+     * @param socialSecurityNumber The social security number of the employee.
+     */
+    public Employee(String firstname, String lastName, String socialSecurityNumber) {
+        super(firstname, lastName, socialSecurityNumber);
+    }
+
+    /**
+     * Gives a printable {@code String} displaying full name and social security number.
+     *
+     * @return {@code String} employee's full name and social security number.
+     */
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
